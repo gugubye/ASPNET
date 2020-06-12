d09a816bf38dbf7cc3385d39a27f1382601277a4
gugubye <512203915@qq.com>
2020-06-12 15:12:06 +0800

    Commit Message

---
+++ AspEFCore/AspEFCore.Data/Mycontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AspEFCore.Domain;
+
+namespace AspEFCore.Data
+{
+    public class Mycontext : DbContext
+    {
+
+        public Mycontext(DbContextOptions<Mycontext> options):base(options)
+        {
+
+        }
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
---
+++ AspEFCore/AspEFCore.Domain/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspEFCore.Domain
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AreaCore { get; set; }
+        public int ProvinceId { get; set; }
+        public Province Province { get; set; }
+    }
+}
